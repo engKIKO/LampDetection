853851f55101674ae4ef599b064a85fedb650437
Eng21072546 <thanaphat.boo@student.mahidol.edu>
2025-04-25 10:45:57 +0700

    Add json log function

--- lampLogger.py
+++ lampLogger.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import json
 
 # Custom night time range
 NIGHT_START = 18  # 6 PM
@@ -14,4 +15,15 @@ def log_broken_lamp(lamp_id, label, status="broken",reason="majority 'off' detec
     with open(log_path, "a") as log_file:
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         log_file.write(f"[{timestamp}] Lamp {lamp_id} is OFF during night. Cam_detection: {label} status:{status} Reason:{reason}\n")
-        return f"[{timestamp}] Lamp {lamp_id} is OFF during night. Cam_detection: {label} status:{status} Reason:{reason}\n"
+        # return f"[{timestamp}] Lamp {lamp_id} is OFF during night. Cam_detection: {label} status:{status} Reason:{reason}\n"
+        return log_json(lamp_id=lamp_id,label=label,status=status,reason=reason)
+
+def log_json(lamp_id, label, status="broken",reason="majority 'off' detections during night"):
+    jsonString = {
+        "Lamp_id" :lamp_id,
+        "lablel" : label,
+        "status" :status,
+        "reson" :reason,
+        "time" : datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    }
+    return json.dumps(jsonString)
