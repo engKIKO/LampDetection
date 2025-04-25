9ba6b7051172e0d9c836bd185cdfe7dd6c4c4660
Eng21072546 <thanaphat.boo@student.mahidol.edu>
2025-04-25 11:03:42 +0700

    Add log function support influx db

--- lampLogger.py
+++ lampLogger.py
@@ -1,5 +1,15 @@
 from datetime import datetime
 import json
+from influxdb_client import InfluxDBClient, Point, WriteOptions
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+bucket = "LampLogs"
+org = "Mahidol"
+token = "PImIAf8HD9QOlfCcuHR_5B6DN6lgrK_s0Q5GK4ihgcehoJZQgm16V5lrjzeYfUfu4YaLpTJhVtybr-StrAi7lA=="
+url = "http://localhost:8086"
+
+client = InfluxDBClient(url=url, token=token, org=org)
+write_api = client.write_api(write_options=SYNCHRONOUS)
 
 # Custom night time range
 NIGHT_START = 18  # 6 PM
@@ -27,3 +37,13 @@ def log_json(lamp_id, label, status="broken",reason="majority 'off' detections d
         "time" : datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     }
     return json.dumps(jsonString)
+
+def log_lamp_status(lamp_id, status, confidence, ldr_value,level="Info"):
+    point = Point("lamp_status") \
+        .tag("lamp_id", lamp_id) \
+        .tag("level",level)\
+        .field("status", status) \
+        .field("confidence", confidence) \
+        .field("ldr", ldr_value) \
+        .time(datetime.now())
+    write_api.write(bucket=bucket, org=org, record=point)
